04b4e10ff90e4d691f985fae4d1b9e94b856ca18
Woan-Nu Yao <woannu.y@gmail.com>
2022-10-10 20:16:06 -0700

    assinged variables used in generating password

--- Develop/script.js
+++ Develop/script.js
@@ -24,5 +24,20 @@ generateBtn.addEventListener("click", writePassword);
  */
 function generatePassword() {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
+  // Lowercser
+  // Uppercase
+  // numeric
+  // special characters
+var numCharacter = Number (prompt ("How many characters would you like your password to contain? Chose between 8 - 128."));
+var lowerCase = ["a", "b", "c", "d", "e", "f", 'g', "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; //26
+var upperCase = ["A", "B", "C", "D", "E", "F", 'G', "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; //26
+var numericnum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; //10
+var specialChar = ["@", "#", "*", "!", "&", "5"]; //6
+
+ 
+  
+//validate input
+//gerate password per the criteria input
+//display password onto the page
 
 }
